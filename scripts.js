0c1790e72df40c4ddfcd85b33ac46434adb4f20d
godigitinsurance <59437973+godigitinsurance@users.noreply.github.com>
2020-08-14 18:25:33 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+$("#customerName").text("Suman");
+$("#policyNumber").text("NJ01104450");
+$("#startDate").text("14-Aug-2020");
+$("#endDate").text("15-Aug-2021");
+
+
+$("#type").text("Two Wheeler");
+$("#registrationNumber").text("PB08BZ6880");
+$("#make").text("TVS");
+$("#model").text("WEGO");
+$("#vehicalSubType").text("WEGO");
+$("#engineNo").text("OG4NB1271107");
+$("#chassis").text("MD626AG49BIN77217");
